51d30d61479e3fdb7d1f2b50abd7336124facd3e
BINIL E B <binilbalan.e.k@gmail.com>
2023-05-13 12:18:07 +0530

    add avatar in register page'

--- src/component/Register.js
+++ src/component/Register.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import React,{useRef,useState,useEffect} from 'react'
 import '../style/Register.css'
 function Register() {
+const [avatar,setAvatar] = useState('images/avatar.png')
+const selectedImg = useRef()
+useEffect(()=>{
+
+},[])
+
+function handleSelect(e){
+e.preventDefault()
+document.getElementById('image').click()
+}
+
+function insertImage(e){
+ const imgSelected = e.target.files[0]
+ const imgurl = URL.createObjectURL(imgSelected)
+ setAvatar(imgurl)
+}
   return (
     <>
     <div className="bg-color">
@@ -10,8 +26,8 @@ function Register() {
       </div>
 
       <section className="register-container">
-          <div className="login">
-            <h2 className="login-h3">Login</h2>
+          <div className="register">
+            <h2 className="register-h3">Sign up</h2>
        <form className="input-form">
        <div className="login-input-container">
               <input type="email"  className="login-input" placeholder="Username"/>
@@ -22,8 +38,13 @@ function Register() {
             <div className="login-input-container">
               <input type="password" className="login-input" placeholder="Password"/>
             </div>
+            <div className="register-btn-container2">
+              <img className='register-avatar' width='50px' src={avatar} alt="" />
+              <input type="file" accept="image/*" id='image' className='img-file' onChange={insertImage}/>
+              <button type="button" className="avatar-btn" onClick={handleSelect}><i className="fa-solid fa-image"></i></button>
+            </div>
             <div className="register-btn-container">
-              <button type="submit" className="register-btn">Register</button>
+              <button type="submit" className="register-btn" >Register</button>
             </div>
        </form>
        <div className="or">Or</div>
