dc2267417623841689d40f5280bcfa1640e95e13
BINIL E B <binilbalan.e.k@gmail.com>
2023-06-01 20:58:43 +0530

    small edit in version 1.1

--- src/page/Home.js
+++ src/page/Home.js
@@ -217,7 +217,7 @@ function Home() {
         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div className="modal-body">
-       Are you sure want to quit 
+      Are you sure you want to quit?
       </div>
       <div className="modal-footer border-0">
         <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">No</button>
