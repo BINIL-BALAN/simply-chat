a7e412d3f0835e7faa4d1e59862f4373513e00d8
BINIL E B <binilbalan.e.k@gmail.com>
2025-03-16 18:24:43 +0530

    comments removed

--- src/page/Register.js
+++ src/page/Register.js
@@ -242,13 +242,3 @@ function Register() {
 }
 
 export default Register;
-
-// .then((userCredential) => {
-//   const user = userCredential.user;
-//   console.log(user);
-// })
-// .catch((error) => {
-//   const errorCode = error.code;
-//   const errorMessage = error.message;
-//   console.log(errorMessage);
-// });
